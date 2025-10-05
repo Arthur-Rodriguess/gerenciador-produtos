784f86220269bbbde544301eb131733bf4c6f99c
Arthur-Rodriguess <artrod150309@gmail.com>
2025-10-05 14:10:45 -0300

    active link background

--- public/includes/aside.php
+++ public/includes/aside.php
@@ -1,7 +1,8 @@
 <aside class="sidebar">
+    <?php $currentPage = basename($_SERVER['PHP_SELF']);?>
     <nav class="sidebar__links" id="sidebar">
-        <a href="/register.php" class="sidebar__link">Registrar Produto</a>
-        <a href="/read.php" class="sidebar__link">Ver Produtos</a>
-        <a href="/api/create_pdf.php" class="sidebar__link">Baixar PDF</a>
+        <a href="/register.php" class="sidebar__link <?= $currentPage === 'register.php' ? 'active' : ''?>">Registrar Produto</a>
+        <a href="/read.php" class="sidebar__link <?= $currentPage === 'read.php' ? 'active' : ''?>">Ver Produtos</a>
+        <a href="/api/create_pdf.php" class="sidebar__link <?= $currentPage === 'create_pdf.php' ? 'active' : ''?>">Baixar PDF</a>
     </nav>
 </aside>
